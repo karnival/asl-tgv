d6ebc21c936541fdf3fec6a577f0bb79229a21e5
karnival <david@calpol.us>
2017-11-15 15:59:20 +0000

    Add early draft of main.py
    This is only intended to give an overview of what main.py will
    eventually include. It isn't even runnable yet, let alone right.

---
+++ main.py
@@ -0,0 +1,43 @@
+import nibabel as nib
+import numpy as np
+
+
+def update_step(P, Q, p, r, q, s, u_c, u_l, v, w, ubar_c, ubar_l, vbar, wbar):
+    P_new = Pfun(lambd, P + sigma*(stack(ubar_c) - u_c_delta))
+    Q_new = Pfun(lambd, Q + sigma*(stack(ubar_l) - u_l_delta))
+
+    p_new = Pfun(gamma1 * alpha1, p + sigma*(grad(ubar_l) - vbar))
+    r_new = Pfun(gamma2 * alpha1, r + sigma*(grad(ubar_c - ubar_l) - wbar))
+    q_new = Pfun(gamma1 * alpha0, q + sigma*epsilon(vbar))
+    s_new = Pfun(gamma2 * alpha0, s + sigma*epsilon(wbar))
+
+    u_c_new = u_c + tau * (div(r_new) - stack(P_new))
+    u_l_new = u_l + tau * (div(p_new - r_new) - stack(Q_new))
+
+    v_new = v + tau * (p_new + div(q_new))
+    w_new = w + tau * (r_new + div(s_new))
+
+    ubar_c_new = 2*u_c_new - u_c
+    ubar_l_new = 2*u_l_new - u_l
+
+    vbar_new = 2*v_new - v
+    wbar_new = 2*w_new - w
+
+
+def Pfun(a, b):
+    for i in range(np.shape(b, 1)):
+        for j in range(np.shape(b, 1)):
+            for k in range(np.shape(b, 1)):
+                to_ret[i,j,k] = b[i,j,k] / max(1, np.norm(b[i,j,:], 2) / a)
+
+    return to_ret
+
+
+def main():
+    filename = 'tmp_asl.nii.gz'
+
+    data = nib.load(filename).get_data()
+
+
+if __name__ == '__main__':
+    main()
